cfdbe1c94a3e8a08e58c425b787f09e27ce64e4c
Juan Carlos Avila <juanvs23@gmail.com>
2023-08-31 07:11:56 -0400

    add delete function

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,29 @@ import Form from "./components/form";
 function App() {
   const [items, setItem] = useState<string[]>(["luis", "maria"]);
 
+  const handlerDelete = (item: string) => {
+    const newList = items.filter((i) => i !== item);
+    setItem(newList);
+  };
   return (
     <main className="container">
+      <h1 className="text-center display-2 mb-4">Listing</h1>
       <div className="row">
         <div className="col-md-6">
           <ul className="list-group">
             <li className="list-group-item active">List</li>
             {items.map((item, index) => (
-              <li className="list-group-item" key={index}>
+              <li
+                className="list-group-item  d-flex justify-content-between "
+                key={index}
+              >
                 {item}
+                <button
+                  onClick={() => handlerDelete(item)}
+                  className="badge bg-danger btn rounded-pill"
+                >
+                  Delete
+                </button>
               </li>
             ))}
           </ul>
